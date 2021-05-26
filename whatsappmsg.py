1a8a964b903c5def2635e3ff3b7238b20b3f1eb1
L-O-K-E-S-H-P <84820086+L-O-K-E-S-H-P@users.noreply.github.com>
2021-05-26 18:40:41 +0530

    Update whatsappmsg.py

--- whatsappmsg.py
+++ whatsappmsg.py
@@ -1,2 +1,2 @@
 import pywhatkit
-pywhatkit.sendwhatmsg('+91 8608675432','Hi keerthi i am sending message to u at 7.58',19,58)
+pywhatkit.sendwhatmsg('+91 8608675432','Life is very short nanba always be happy',12,0)
