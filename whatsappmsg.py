66325967b25c28e0dc7453690d594b4b7ac82ff1
L-O-K-E-S-H-P <84820086+L-O-K-E-S-H-P@users.noreply.github.com>
2021-08-18 12:11:42 +0530

    Update whatsappmsg.py

--- whatsappmsg.py
+++ whatsappmsg.py
@@ -1,2 +1,2 @@
 import pywhatkit
-pywhatkit.sendwhatmsg('+91 8608675432','Life is very short nanba always be happy',12,0)
+pywhatkit.sendwhatmsg('+91 8608675432','Sending you best wishes for success, health, and good fortune today and in the year to come. Enjoy your special day. Happy Birthday! Thank you for always being there for me and never giving up on me, Dad.',12,0)
